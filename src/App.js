fcd7eae74799a9cad85c58404bda655db25d75f7
Mahmud Bhanapatel <mahmud.bhanapatel@brighthr.com>
2023-05-01 14:56:03 +0100

    format results into a table

--- src/App.js
+++ src/App.js
@@ -21,10 +21,11 @@ const App = () => {
 	};
 
 	return (
-		<div>
-			github access
-			<div>
+		<div className='p-8 space-y-4'>
+			<h1>Github repository search</h1>
+			<div className='space-x-4'>
 				<input
+					className='px-2 py-1 border border-gray-500 rounded-lg'
 					onChange={(e) => setSearch(e.target.value)}
 					onKeyDown={(e) => {
 						if (e.key === 'Enter' && search) handleSubmit(e);
@@ -35,8 +36,27 @@ const App = () => {
 				</button>
 			</div>
 			<div>
-				{result &&
-					result.map((item) => <Details key={item.id} item={item} />)}
+				{result && (
+					<table className='w-full border'>
+						<thead>
+							<tr className='border'>
+								<th>Name</th>
+								<th>Author</th>
+								<td>forks</td>
+								<td>watchers</td>
+								<td>stars</td>
+								<td>open issues</td>
+							</tr>
+						</thead>
+						{result.map((item) => (
+							<tbody>
+								<tr className='border'>
+									<Details key={item.id} item={item} />
+								</tr>
+							</tbody>
+						))}
+					</table>
+				)}
 			</div>
 		</div>
 	);
--- src/Details.js
+++ src/Details.js
@@ -4,25 +4,35 @@ const Details = ({ item }) => {
 	const [expanded, setExpanded] = useState(false);
 
 	return (
-		<div>
-			<div className='flex space-x-6'>
-				<a href={item.html_url} target='_blank' rel='noreferrer'>
+		<>
+			<td>
+				<a
+					className='text-green-700 hover:underline hover:text-green-500 active:text-blue-900'
+					href={item.html_url}
+					target='_blank'
+					rel='noreferrer'
+				>
 					{item.name}
 				</a>
-				<div>{item.owner.login}</div>
-				<button onClick={() => setExpanded(!expanded)}>
-					{expanded ? 'Less details' : 'More details'}
-				</button>
-			</div>
-			{expanded && (
+			</td>
+			<td>{item.owner.login}</td>
+			<td>{item.forks}</td>
+			<td>{item.watchers}</td>
+			<td>{item.stargazers_count}</td>
+			<td>{item.open_issues_count}</td>
+			<td>open README</td>
+			{/* <button onClick={() => setExpanded(!expanded)}>
+				{expanded ? 'Less details' : 'More details'}
+			</button> */}
+			{/* {expanded && (
 				<div className='flex space-x-6'>
 					<div>{item.forks}</div>
-					<div>likes</div>
+					<div>{item.watchers}</div>
 					<div>{item.stargazers_count}</div>
 					<div>{item.open_issues_count}</div>
 				</div>
-			)}
-		</div>
+			)} */}
+		</>
 	);
 };
 
